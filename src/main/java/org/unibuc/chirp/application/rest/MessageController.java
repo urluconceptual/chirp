5982c66b22a7350dd9044bfe89bc22c681352b38
Alexandra-Diana Ciocan <ciocan_alexandra16@yahoo.com>
2025-05-23 17:10:47 +0300

    fix: unused import

--- src/main/java/org/unibuc/chirp/application/rest/MessageController.java
+++ src/main/java/org/unibuc/chirp/application/rest/MessageController.java
@@ -1,7 +1,6 @@
 package org.unibuc.chirp.application.rest;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.unibuc.chirp.domain.service.MessageService;
